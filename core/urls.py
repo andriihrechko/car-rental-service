7e5869f8aa519a87d4271b0039aee1830fb8bd1b
andriihrechko <andriihrechko250703@gmail.com>
2026-06-13 01:41:47 +0300

    refactor users urls and add swagger documentation

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,22 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/service/", include("users.urls", namespace="users")),
+    path("admin/", admin.site.urls),
+    path("api/service/users/", include("users.urls", namespace="users")),
+    path(
+        "api/schema/",
+        SpectacularAPIView.as_view(),
+        name="schema"
+    ),
+    # Optional UI:
+    path(
+        "api/schema/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger",
+    ),
 ]
 
 if settings.DEBUG:
--- users/urls.py
+++ users/urls.py
@@ -10,8 +10,8 @@ from users.views import SignUpView, UserMeView
 app_name = "users"
 
 urlpatterns = [
-    path("users/", SignUpView.as_view(), name="signup"),
-    path("users/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("users/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("users/me/", UserMeView.as_view(), name="me"),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", UserMeView.as_view(), name="me"),
 ]
