de6ae37e7c5f6d1f4df3f5046d593968c53b9b78
andriihrechko <andriihrechko250703@gmail.com>
2026-06-12 11:36:56 +0300

    add custom user model with email and register it in admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    """Define custom admin pages for users."""
+
+    list_display = ("email", "is_staff", "is_active")
+    search_fields = ("email",)
+
+    list_filter = ("is_staff", "is_superuser", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+    ordering = ('email',)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+from users.managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username."""
+    username = None
+    email = models.EmailField(unique=True, max_length=255, db_index=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
