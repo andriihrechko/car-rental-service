c283dc07c93c1533a4cc0de59375032ec74d04f4
Alisa Hoys <hoys@protonmail.com>
2026-06-13 13:21:58 +0200

    add Car model with migrations

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class FuelType(models.TextChoices):
+    GAS = "GAS", "Gas"
+    DIESEL = "DIESEL", "Diesel"
+    HYBRID = "HYBRID", "Hybrid"
+    ELECTRIC = "ELECTRIC", "Electric"
+
+
+class Car(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.PositiveSmallIntegerField()
+    fuel_type = models.CharField(max_length=10, choices=FuelType.choices)
+    daily_rate = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.year})"
