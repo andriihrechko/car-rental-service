22a91440ea379c20cc39fac43cc4c124f40f56f1
Oleksii Prokopenko <oleksijprokopenko@gmail.com>
2026-06-14 15:15:20 +0300

    setup ruff and configure it
    - update requirements.txt
    
    - add ruff configuration
    
    - fix ruff lint issues

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
-from django.conf import settings
-from django.contrib import admin
-from django.conf.urls.static import static
-from django.urls import path
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
 
 
 urlpatterns = [
@@ -11,5 +11,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += debug_toolbar_urls()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
