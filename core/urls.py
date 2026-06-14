2196511cb341a6f0617aba1be3d4b348dbd5ca28
andriihrechko <andriihrechko250703@gmail.com>
2026-06-14 15:46:14 +0300

    add include to imports

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 
