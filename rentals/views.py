6c060a1bbfd0e7f2d2a97043c1da5d58497ff83f
Stanislav <staskobetc@gmail.com>
2026-06-15 00:17:22 +0300

    feat(rentals): add RentalViewSet with return and cancel actions

--- rentals/views.py
+++ rentals/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import mixins, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from rentals.models import Rental
+from rentals.serializers import (
+    RentalCancelSerializer,
+    RentalReturnSerializer,
+    RentalSerializer,
+)
+
+
+class RentalViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet,
+):
+    serializer_class = RentalSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        qs = Rental.objects.select_related("car", "user")
+        if not user.is_staff:
+            qs = qs.filter(user=user)
+        user_id = self.request.query_params.get("user_id")
+        status_param = self.request.query_params.get("status")
+        if user_id and user.is_staff:
+            qs = qs.filter(user_id=user_id)
+        if status_param:
+            qs = qs.filter(status=status_param.upper())
+        return qs
+
+    @action(detail=True, methods=["post"], url_path="return")
+    def return_rental(self, request, pk=None):
+        rental = self.get_object()
+        serializer = RentalReturnSerializer(
+            rental, data={}, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(RentalSerializer(rental).data)
+
+    @action(detail=True, methods=["post"], url_path="cancel")
+    def cancel_rental(self, request, pk=None):
+        rental = self.get_object()
+        serializer = RentalCancelSerializer(
+            rental, data={}, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(RentalSerializer(rental).data)
