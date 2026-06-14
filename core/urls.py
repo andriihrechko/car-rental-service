f957162c23e49bb77dc69ca3476a2aae987fac82
andriihrechko <andriihrechko250703@gmail.com>
2026-06-14 15:50:46 +0300

    add spectacular views to imports

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
