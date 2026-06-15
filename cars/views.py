25e637797356b27ce0a627554187b953e09b099a
Alisa Hoys <hoys@protonmail.com>
2026-06-15 10:55:43 +0200

    move CarFilter to filters.py

--- cars/views.py
+++ cars/views.py
@@ -1,28 +1,10 @@
-from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, viewsets
 from rest_framework.filters import SearchFilter
 
+from cars.filters import CarFilter
 from cars.models import Car
 from cars.serializers import CarSerializer
-
-
-class CarFilter(filters.FilterSet):
-    min_year = filters.NumberFilter(field_name="year", lookup_expr="gte")
-    max_year = filters.NumberFilter(field_name="year", lookup_expr="lte")
-    is_available = filters.BooleanFilter(
-        method="filter_available",
-        label="Available"
-    )
-
-    def filter_available(self, queryset, name, value):
-        if value:
-            return queryset.filter(inventory__gt=0)
-        return queryset.filter(inventory=0)
-
-    class Meta:
-        model = Car
-        fields = ["brand", "fuel_type", "min_year", "max_year", "is_available"]
 
 
 class CarViewSet(viewsets.ModelViewSet):
