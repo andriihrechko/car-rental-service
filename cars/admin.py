cce888f5c7e7467eea60642c568ed79d1e20ec08
andriihrechko <andriihrechko250703@gmail.com>
2026-06-16 14:28:33 +0300

    add car to admin and customize

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from cars.models import Car
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = (
+        "brand",
+        "model",
+        "year",
+        "daily_rate",
+        "fuel_type",
+        "inventory",
+    )
+    list_filter = ("fuel_type", "year")
+    search_fields = ("brand", "model")
