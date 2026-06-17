7cba38a7c906bdc79d88840f988b2a4520b93fe9
andriihrechko <andriihrechko250703@gmail.com>
2026-06-17 09:26:26 +0300

    switch to postgres

--- core/settings.py
+++ core/settings.py
@@ -90,23 +90,23 @@ TEMPLATES = [
 WSGI_APPLICATION = "core.wsgi.application"
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": env.str("POSTGRES_DB"),
-#         "USER": env.str("POSTGRES_USER"),
-#         "PASSWORD": env.str("POSTGRES_PASSWORD"),
-#         "HOST": env.str("POSTGRES_HOST"),
-#         "PORT": env.int("POSTGRES_PORT"),
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("POSTGRES_DB"),
+        "USER": env.str("POSTGRES_USER"),
+        "PASSWORD": env.str("POSTGRES_PASSWORD"),
+        "HOST": env.str("POSTGRES_HOST"),
+        "PORT": env.int("POSTGRES_PORT"),
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
