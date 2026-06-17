a2f7ae964efcc830ae06c16de28c1c5e774c0c98
zwirok-git <oleksijprokopenko@gmail.com>
2026-06-17 13:11:47 +0300

    improve notification tests

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -5,40 +5,42 @@ from django.utils import timezone
 from notifications.notification_service import NotificationService
 from payments.models import Payment
 from rentals.models import Rental
-from users.models import User
 
 
 @shared_task
-@transaction.atomic
 def notify_overdue_rentals():
-    overdue_rentals = Rental.objects.select_related("user").filter(
-        actual_return_date__isnull=True,
-        end_date__lt=timezone.now().date(),
-        status=Rental.Status.BOOKED,
-    )
-
-    if not overdue_rentals:
-        return
-
-    overdue_rentals.update(
-        status=Rental.Status.OVERDUE,
-    )
-
-    message = (
-        f"🚨 Overdue rentals report\n\n"
-        f"Total overdue rentals: {overdue_rentals.count()}\n\n"
-    )
-
-    for rental in overdue_rentals:
-        user_first_name = rental.user.first_name
-        user_last_name = rental.user.last_name
-        user_email = rental.user.email
-
-        message += (
-            f"Rental: {rental}\n"
-            f"User:"
-            f" {user_first_name} {user_last_name} - {user_email}\n\n"
+    with transaction.atomic():
+        overdue_rentals = list(
+            Rental.objects.select_related("user").filter(
+                actual_return_date__isnull=True,
+                end_date__lt=timezone.now().date(),
+                status=Rental.Status.BOOKED,
+            )
         )
+
+        if not overdue_rentals:
+            return
+
+        Rental.objects.filter(
+            id__in=[rental.id for rental in overdue_rentals]
+        ).update(
+            status=Rental.Status.OVERDUE,
+        )
+
+        message = (
+            "🚨 Overdue rentals report\n\n"
+            f"Total overdue rentals: {len(overdue_rentals)}\n\n"
+        )
+
+        for rental in overdue_rentals:
+            rental.status = Rental.Status.OVERDUE
+            user = rental.user
+
+            message += (
+                f"Rental: {rental}\n"
+                f"User info: {user.first_name} {user.last_name}\n"
+                f"User email: {user.email}\n\n"
+            )
 
     NotificationService.send(message)
 
@@ -50,15 +52,14 @@ def send_payment_notification(payment_id: int):
         "rental__user",
     ).get(id=payment_id)
 
-    user_first_name = payment.rental.user.first_name
-    user_last_name = payment.rental.user.last_name
-    user_email = payment.rental.user.email
+    user = payment.rental.user
 
     message = (
         "💰 Payment successful\n\n"
         f"Payment ID: {payment.id}\n"
         f"Rental: {payment.rental}\n"
-        f"User: {user_first_name} {user_last_name} - {user_email}\n"
+        f"User info: {user.first_name} {user.last_name}\n"
+        f"User email: {user.email}\n"
         f"Amount: {payment.money_to_pay}"
     )
 
