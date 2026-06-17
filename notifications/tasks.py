40d9d73cb3ac21a4a9a1ea67ed2791e01a51b3e3
zwirok-git <oleksijprokopenko@gmail.com>
2026-06-17 11:32:47 +0300

    update tasks
    - add transaction to notify_overdue_rentals
    - add more details to message in send_payment_notifications

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -1,14 +1,24 @@
 from celery import shared_task
+from django.db import transaction
 from django.utils import timezone
 
 from notifications.notification_service import NotificationService
+from payments.models import Payment
+from rentals.models import Rental
+from users.models import User
 
 
 @shared_task
-def notify_overdue_rentals(rental):
-    overdue_rentals = rental.objects.filter(
+@transaction.atomic
+def notify_overdue_rentals():
+    overdue_rentals = Rental.objects.filter(
         actual_return_date__isnull=True,
         end_date__lt=timezone.now().date(),
+        status=Rental.Status.BOOKED,
+    )
+
+    overdue_rentals.update(
+        status=Rental.Status.OVERDUE,
     )
 
     if not overdue_rentals.exists():
@@ -20,23 +30,27 @@ def notify_overdue_rentals(rental):
     )
 
     for rental in overdue_rentals:
-        message += f"Rental ID: {rental.id}\nUser ID: {rental.user_id}\n\n"
+        message += f"Rental: {rental}\nUser ID: {rental.user_id}\n\n"
 
     NotificationService.send(message)
 
 
 @shared_task
-def send_payment_notification(payment, payment_id: int):
-    payment = payment.objects.select_related(
+def send_payment_notification(payment_id: int):
+    payment = Payment.objects.select_related(
         "rental",
         "rental__user",
     ).get(id=payment_id)
 
+    user_first_name = payment.rental.user.first_name
+    user_last_name = payment.rental.user.last_name
+    user_email = payment.rental.user.email
+
     message = (
         "💰 Payment successful\n\n"
         f"Payment ID: {payment.id}\n"
-        f"Rental ID: {payment.rental.id}\n"
-        f"User ID: {payment.rental.user.id}\n"
+        f"Rental: {payment.rental}\n"
+        f"User: {user_first_name} {user_last_name} - {user_email}\n"
         f"Amount: {payment.money_to_pay}"
     )
 
